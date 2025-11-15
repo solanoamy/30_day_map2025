0fe4a8b7f5762b6f90d40f1603ec63c599d0f356
solanoamy <70601985+solanoamy@users.noreply.github.com>
2025-11-15 00:37:23 -0500

    add lines connecting points to labels
    finally figured out how to use ggrepel with ggtext yay

--- scripts/day8_urban.R
+++ scripts/day8_urban.R
@@ -51,21 +51,22 @@ venues.sf <-
 
 
 venues.sf$label <- c("<span style = 'font-size:6pt'>**Emirates**</span> <img src='https://upload.wikimedia.org/wikipedia/en/thumb/5/53/Arsenal_FC.svg/1021px-Arsenal_FC.svg.png'
-                     height='8' /><br><span style = 'font-size:4pt'>Population density: 16,088 / sq km</span>",
+                     height='9' /><br><span style = 'font-size:4pt'>Population density: 16,088 / sq km</span>",
                      "<span style = 'font-size:6pt'>**Stamford Bridge**</span> <img src='https://upload.wikimedia.org/wikipedia/sco/thumb/c/cc/Chelsea_FC.svg/480px-Chelsea_FC.svg.png'
-                     width='10' /><br><span style = 'font-size:4pt'>Population density 16,509 / sq km</span>",
+                     height='9' /><br><span style = 'font-size:4pt'>Population density 16,509 / sq km</span>",
                      "<span style = 'font-size:6pt'>**Tottenham Hotspur Stadium**</span> <img src='https://upload.wikimedia.org/wikipedia/en/thumb/b/b4/Tottenham_Hotspur.svg/584px-Tottenham_Hotspur.svg.png'
-                     height='8' /><br><span style = 'font-size:4pt'>Population density: 6,966 / sq km</span>",
+                     height='9' /><br><span style = 'font-size:4pt'>Population density: 6,966 / sq km</span>",
                      "<span style = 'font-size:6pt'>**London Stadium**</span> <img src='https://upload.wikimedia.org/wikipedia/en/thumb/c/c2/West_Ham_United_FC_logo.svg/1079px-West_Ham_United_FC_logo.svg.png'
-                     height='8' /><br><span style = 'font-size:4pt'>Population density: 6,518 / sq km</span>",
+                     height='9' /><br><span style = 'font-size:4pt'>Population density: 6,518 / sq km</span>",
                      "<span style = 'font-size:6pt'>**Craven Cottage**</span> <img src='https://cdn.freebiesupply.com/logos/large/2x/fulham-fc-1-logo-png-transparent.png'
-                     width='10' /><br><span style = 'font-size:4pt'>Population density: 7,090 / sq km</span>",
+                     height='9' /><br><span style = 'font-size:4pt'>Population density: 7,090 / sq km</span>",
                      "<span style = 'font-size:6pt'>**Selhurst Park**</span> <img src='https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/Crystal_Palace_FC_logo_%282022%29.svg/962px-Crystal_Palace_FC_logo_%282022%29.svg.png'
-                     height='8' /><br><span style = 'font-size:4pt'>Population density: 7,681 / sq km</span>",
+                     height='9' /><br><span style = 'font-size:4pt'>Population density: 7,681 / sq km</span>",
                      "<span style = 'font-size:6pt'>**Gtech Community Stadium**</span> <img src='https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/Brentford_FC_crest.svg/1200px-Brentford_FC_crest.svg.png'
-                     width='10' /><br><span style = 'font-size:4pt'>Population density: 10,303 / sq km</span>",
+                     height='9' /><br><span style = 'font-size:4pt'>Population density: 10,303 / sq km</span>",
                      "<span style = 'font-size:6pt'>**Wembley Stadium**</span> <img src='https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Flag_of_England.svg/330px-Flag_of_England.svg.png'
-                     width='10' /><br><span style = 'font-size:4pt'>Population density: 7,574 / sq km</span>")
+                     height='9' /><br><span style = 'font-size:4pt'>Population density: 7,574 / sq km</span>")
+
 
 ggplot()+
   geom_sf(data=st_union(real_london), fill = "transparent", lwd=1, color="#360f0f")+
@@ -74,31 +75,20 @@ ggplot()+
   scale_fill_gradient(low="#f5f5f5",high="#870c01",
                       name="Persons per sq km",
                       labels=function(x) format(x, big.mark = ",", scientific = FALSE)) +
-  ggrepel::geom_label_repel(data=venues.sf,
-                            aes(label=stadium, geometry=geometry),
-                            stat = "sf_coordinates")
-
-ggplot()+
-  geom_sf(data=st_union(real_london), fill = "transparent", lwd=1, color="#360f0f")+
-  geom_sf(data=real_london, aes(fill=observation), color = "whitesmoke") +
-  geom_sf(data=venues.sf, size = 2) +
-  scale_fill_gradient(low="#f5f5f5",high="#870c01",
-                      name="Persons per sq km",
-                      labels=function(x) format(x, big.mark = ",", scientific = FALSE)) +
-  geom_richtext(lineheight = .1,
-                family = "SF Pro Text",
-                data=venues.sf,
-                aes(label=label, geometry=geometry),
-                stat = "sf_coordinates")+
-  theme(panel.background = element_rect(fill="#f7f7f7"))
-
-ggplot()+
-  geom_sf(data=st_union(real_london), fill = "transparent", lwd=1, color="#360f0f")+
-  geom_sf(data=real_london, aes(fill=observation), color = "whitesmoke") +
-  geom_sf(data=venues.sf, size = 2) +
-  scale_fill_gradient(low="#f5f5f5",high="#870c01",
-                      name="Persons per sq km",
-                      labels=function(x) format(x, big.mark = ",", scientific = FALSE)) +
+  # make ggrepel text transparent so i can have the lines leading to the point with the rich text features
+  ggrepel::geom_text_repel(lineheight = .1,
+                            family = "SF Pro Text",
+                           # split teams into two sections (those on top and on bottom) to reposition the labels
+                            data=venues.sf %>%
+                              filter(team %in% c("Arsenal","Tottenham","Wembley","West Ham")),
+                            aes(label=team, geometry=geometry),
+                            color = "transparent",
+                            segment.colour="black",
+                            stat = "sf_coordinates",
+                            position = position_nudge_repel(
+                              x = c(0, 0.18, 0.18, -0.05),
+                              y = c(0.13, 0.12, -0.1, 0.1)
+                            ))+
   geom_richtext(lineheight = .1,
                 family = "SF Pro Text",
                 data=venues.sf %>%
@@ -106,9 +96,21 @@ ggplot()+
                 aes(label=label, geometry=geometry),
                 stat = "sf_coordinates",
                 position = position_nudge_repel(
-                  x = c(0, 0.18, 0.2, -0.05),
-                  y = c(0.12, 0.2, -0.1, 0.1)
+                  x = c(0, 0.18, 0.18, -0.05),
+                  y = c(0.13, 0.12, -0.1, 0.1)
                 ))+
+  ggrepel::geom_text_repel(lineheight = .1,
+                            family = "SF Pro Text",
+                            data=venues.sf %>%
+                              filter(team %notin% c("Arsenal","Tottenham","Wembley","West Ham")),
+                            aes(label=team, geometry=geometry),
+                            stat = "sf_coordinates",
+                            color = "transparent",
+                            segment.colour="black",
+                            position = position_nudge_repel(
+                              x = c(0.1, -0.2, 0.2, -0.15),
+                              y = c(-0.1, -0.1, -0.1, 0)
+                            ))+
   geom_richtext(lineheight = .1,
                family = "SF Pro Text",
                data=venues.sf %>%
@@ -119,7 +121,10 @@ ggplot()+
                  x = c(0.1, -0.2, 0.2, -0.15),
                  y = c(-0.1, -0.1, -0.1, 0)
                ))+
-  theme(panel.background = element_rect(fill="#f7f7f7"))
+  theme(panel.background = element_rect(fill="#f7f7f7"),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank())
 
 
 
